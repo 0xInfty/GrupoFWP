e13c0720ff99668f48f9463deb5b84f223859e21
0xInfty <anirix.vall@gmail.com>
2018-08-22 16:27:11 -0300

    Modifico play.py. Antes era 'record'

--- play.py
+++ play.py
@@ -1,44 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Conexión JACK
-
-"""
+"""PyAudio Example: Play a WAVE file."""
 
 import pyaudio
 import wave
+import sys
 
 CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+
+if len(sys.argv) < 2:
+    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
+    sys.exit(-1)
+
+wf = wave.open(sys.argv[1], 'rb')
 
 p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
 
-print("* recording")
+data = wf.readframes(CHUNK)
 
-frames = []
-
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-
-print("* done recording")
+while data != '':
+    stream.write(data)
+    data = wf.readframes(CHUNK)
 
 stream.stop_stream()
 stream.close()
-p.terminate()
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+p.terminate()
