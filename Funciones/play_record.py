ffa41bed20476d99df73683c9e97420adbcd402e
0xInfty <anirix.vall@gmail.com>
2018-08-29 12:02:14 -0300

    Modifico la función play_record. Pero todavía no creo que funcione

--- Funciones/play_record.py
+++ Funciones/play_record.py
@@ -8,7 +8,17 @@ Created on Tue Aug 28 18:34:02 2018
 @author: mfar
 """
 
-def play_record():
+from pyaudio import paInt16
+
+def play_record(fname='output', 
+    #A = 1, # range [0,1]
+    T = 1,
+    #n = 200
+    dt = 0.005,
+    m = 3, # cantidad de veces a repetir el buffer
+    sr = 44000,
+    ch = 2,
+    ft = paInt16):
 
     import pyaudio
     import numpy as np
@@ -17,17 +27,7 @@ def play_record():
     
     p = pyaudio.PyAudio()
     
-    A = 1 # range [0,1]
-    T = 1
-    #n = 200
-    dt = 0.005
-    m = 3 # cantidad de veces a repetir el buffer
-    sr = 44000
-    ch = 2
-    ft = pyaudio.paInt16
-    fname='output'
-    
-    frames = []
+    s_in = []
     
     print("Longitud del buffer: %i" % int(T/dt))
     if int(T/dt) != T/dt:
@@ -36,56 +36,37 @@ def play_record():
         print("Intervalo redefinido a %f s" % dt)
     
     n = int(T/dt)
-    samples = np.zeros(n)
+    s_out = np.zeros(n)
     
-    samples[0:n//2+1] = np.linspace(0,1,n//2+1)
-    samples[n//2:n] = np.linspace(1,0,n//2+1)[0:n//2]
+    s_out[0:n//2+1] = np.linspace(0,1,n//2+1)
+    s_out[n//2:n] = np.linspace(1,0,n//2+1)[0:n//2]
     
-    samples = samples.astype(np.float32)
+    s_out = s_out.astype(np.float32)
     
     def callback(in_data, frame_count, time_info, status):
-
-        streamrecord = p.open(format=ft,
-            channels=ch,
-            rate=sr,
-            input=True,
-            frames_per_buffer=n)
-        
-        print("* recording")
-        
-        for i in range(0, int(sr / n * m * T)):
-            data = streamrecord.read(n)
-            frames.append(data)
-        print("* done recording")
-           
-        streamrecord.stop_stream()
-        streamrecord.close()
-        outputdata = samples
-
-        return (outputdata, pyaudio.paContinue)
+        return (in_data, pyaudio.paContinue)
     
     
     # for paFloat32 sample values must be in range [-1.0, 1.0]
-    streamplay = p.open(format=ft,
+    stream = p.open(format=ft,
                     channels=ch,
                     rate=sr,
+                    input=True,
                     output=True,
+                    input_device_index=1,
+                    output_device_index=3,
+                    frames_per_buffer=n,
                     stream_callback = callback)
     
-    
-    # play. May repeat with different volume values (if done interactively) 
-    # start the stream (4)
-    streamplay.start_stream()
+    stream.start_stream()
     
     
     # wait for stream to finish (5)
-    for i in range(10):
-        if streamplay.is_active()==True:
-            time.sleep(0.1)
-            i=i+1
+    while stream.is_active()==True:
+        time.sleep(0.1)
     
-    streamplay.stop_stream()
-    streamplay.close()
+    stream.stop_stream()
+    stream.close()
        
     p.terminate()
     
@@ -93,5 +74,5 @@ def play_record():
     wf.setnchannels(ch)
     wf.setsampwidth(p.get_sample_size(ft))
     wf.setframerate(sr)
-    wf.writeframes(b''.join(frames))
+    wf.writeframes(b''.join(s_in))
     wf.close()
