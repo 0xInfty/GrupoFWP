cf5b0c6b8b5c9bd4cc27be552c6324295ecc968b
0xInfty <anirix.vall@gmail.com>
2018-08-29 16:00:34 -0300

    Agrego en master la función play_record tal cual está en el commit 90892f7 de branchvale

--- Funciones/play_record.py
+++ Funciones/play_record.py
@@ -11,23 +11,27 @@ Created on Tue Aug 28 18:34:02 2018
 from pyaudio import paInt16
 
 def play_record(fname='output', 
-    #A = 1, # range [0,1]
-    T = 1,
-    #n = 200
-    dt = 0.005,
+    #A = 1, # amplitud, range [0,1]
+    T = 1, # duración del buffer
+    #n = 200 # cantidad de frames en un buffer = chunk
+    dt = 0.005, # duración de cada frame del buffer
     m = 3, # cantidad de veces a repetir el buffer
-    sr = 44000,
-    ch = 2,
-    ft = paInt16):
+    sr = 44000, # sample rate
+    ch = 2, # cantidad de canales tanto de input como de output
+    ft = paInt16, # formato de input
+    freq = 440,
+    mode = 'saw', # modo de input "saw" o "sine"
+    savewav = True,
+    savetext = False,
+    returntxt = False): 
 
     import pyaudio
-    import numpy as np
-    import time
+    from numpy import linspace, float32
     import wave
     
     p = pyaudio.PyAudio()
     
-    s_in = []
+    s_f = []
     
     print("Longitud del buffer: %i" % int(T/dt))
     if int(T/dt) != T/dt:
@@ -36,37 +40,60 @@ def play_record(fname='output',
         print("Intervalo redefinido a %f s" % dt)
     
     n = int(T/dt)
-    s_out = np.zeros(n)
     
-    s_out[0:n//2+1] = np.linspace(0,1,n//2+1)
-    s_out[n//2:n] = np.linspace(1,0,n//2+1)[0:n//2]
+    if mode=='sine':
+        from numpy import sin, pi
+        
+        s_0 = sin(2*pi*linspace(0,T,dt)/T)
+        
+    if mode=='freq':
+        from numpy import sin, pi, arange
+        
+        s_0 = sin(2*pi*arange(sr*T)*freq/sr)
+        
+    elif mode=='saw':
+        from numpy import zeros
+        
+        s_0 = zeros(n)
+        
+        s_0[0:n//2+1] = linspace(0,1,n//2+1)
+        s_0[n//2:n] = linspace(1,0,n//2+1)[0:n//2]
     
-    s_out = s_out.astype(np.float32)
+    
+    else:
+        return "¡Error! Modo de onda de output no especificada"
+    
+    s_0 = s_0.astype(float32)
     
     def callback(in_data, frame_count, time_info, status):
-        return (in_data, pyaudio.paContinue)
+        return (s_0, pyaudio.paContinue)
     
+    streamplay = p.open(format=ft,
+                channels=ch,
+                rate=sr,
+                output=True,
+                stream_callback = callback)
+
+    streamrecord = p.open(format=ft,
+                channels=ch,
+                rate=sr,
+                input=True,
+                frames_per_buffer=n)
+
+    streamplay.start_stream()
+    print("* recording")
+    streamrecord.start_stream()
+    for i in range(0, int(sr / n * m * T)):#m * T)):
+        data = streamrecord.read(n)
+        s_f.append(data)
+    print("* done recording")    
     
-    # for paFloat32 sample values must be in range [-1.0, 1.0]
-    stream = p.open(format=ft,
-                    channels=ch,
-                    rate=sr,
-                    input=True,
-                    output=True,
-                    input_device_index=1,
-                    output_device_index=3,
-                    frames_per_buffer=n,
-                    stream_callback = callback)
+
+    streamrecord.stop_stream()
+    streamplay.stop_stream()
     
-    stream.start_stream()
-    
-    
-    # wait for stream to finish (5)
-    while stream.is_active()==True:
-        time.sleep(0.1)
-    
-    stream.stop_stream()
-    stream.close()
+    streamrecord.close()
+    streamplay.close()
        
     p.terminate()
     
@@ -74,5 +101,5 @@ def play_record(fname='output',
     wf.setnchannels(ch)
     wf.setsampwidth(p.get_sample_size(ft))
     wf.setframerate(sr)
-    wf.writeframes(b''.join(s_in))
+    wf.writeframes(b''.join(s_f))
     wf.close()
