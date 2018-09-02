af02e7a5b8eae3748a9090b45b46f006513769f8
0xInfty <anirix.vall@gmail.com>
2018-09-02 12:22:40 -0300

    Abro branch 'vale_waveform' para experimentar. Modifico función 'waveform'. Realizo cambios: importantes de funcionamiento y leves de sintaxis.
    Cambios importantes de funcionamiento:
    	ahora la función toma también un argumento 'n';
    	ahora la función no devuelve una función, sino un array de longitud n que ya tiene la forma adecuada.
    
    Cambios leves de sintaxis:
    	agrego documentación.

--- Funciones/waveform.py
+++ Funciones/waveform.py
@@ -2,19 +2,43 @@
 """
 Created on Wed Aug 29 19:49:50 2018
 
-@author: Usuario
+@author: 0xInfty
 """
 
-def waveform(key):
+def waveform(key, n):
+    """Makes a period of a wave of form 'key' on an array of lenght 'n'.
+    
+This function defines a period of a wave. Its waveform is chosen with \
+the input 'key' which can take a limited number of values: 'sin' for \
+a sine, 'tri' for a triangle wave. Its digital resolution is given by \
+the input 'n' which is the number of values it takes in a period and \
+which is therefore the number of intervals the period is divided into.
 
-    def sin(n):
+Variables:
+>> key (str) [waveform]
+>> n (int) [resolution]
+
+Returns:
+>> wave (array) [period of a wave, array of length n]
+
+Raises:
+- KeyError("Los posibles valores de key son: 'sin', 'tri'") if a wrong \
+waveform key is given.
+
+Beware:
+- The returned waveform is normalized to 1 (its maximum amplitude is 1).
+- The returned waveform is centered on 0 (it's a symmetric function \
+whose minimum amplitude is -1).
+    
+    """
+
+    if key == 'sin':
         
         from numpy import sin, pi, arange
         
         return sin(2*pi*arange(n)/n)
-    
-    
-    def tri(n):
+
+    elif key == 'tri':
         
         from numpy import zeros, linspace
         
@@ -24,12 +48,8 @@ def waveform(key):
         out[n//2:n] = linspace(1,0,n//2+1)[0:n//2]
         
         return out
-    
-    switcher = {'sin': sin, 'tri': tri}
-    
-    if key in switcher:       
-        return switcher.get(key, lambda: "Invalid keyword")
-    
-    else:
+
+    else:        
+
         raise KeyError("Los posibles valores de key son: 'sin', 'tri'.")
         return
