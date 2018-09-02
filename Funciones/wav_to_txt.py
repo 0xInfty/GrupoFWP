34756404d1fe3d4a7dff90d2942ee6394fbc0b65
0xInfty <anirix.vall@gmail.com>
2018-09-02 10:28:01 -0300

    Agrego función 'wav_to_txt'

---
+++ Funciones/wav_to_txt.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 29 19:42:57 2018
+
+@author: 0xInfty
+"""
+
+def wav_to_txt(fname, 
+               fdir='C:\\Users\\Usuario\\Documents\\Git\\Público\\Pais',
+               gdir='C:\\Users\\Usuario\\Documents\\Git\\Público\\Pais'
+               ):
+
+    """File conversion from '.wav' to '.txt'
+    
+Takes the wav file named fname from the fdir directory and converts it \
+to txt.
+    
+Variables:
+>> fname (str) [wav file name, without format]
+>> fdir (str) [wav file directory]
+>> gdir (str) [txt file directory]
+
+Returns: None
+    
+Beware:
+- It takes the file name as fname; i.e. "Hi" and not "Hi.wav".
+- It saves the txt file with fname name; i.e. fname="Hi" converts \
+fdir\\Hi.wav into gdir\\Hi.wav
+    
+    """
+    
+    from numpy import arange, savetxt, transpose, array
+    from scipy.io import wavfile
+    from os import getcwd, makedirs, chdir
+    from os.path import isdir, isfile
+    
+    home = getcwd()
+    chdir(fdir)
+    
+    file = fname + '.wav'
+
+    sr, datos = wavfile.read(file)
+    
+    n = len(datos[:,0])
+    if n > 500000:
+        return "¡Error! Guardar un archivo .txt tan largo lleva mucho tiempo"
+
+    t = arange(0, n/sr, 1/sr)
+
+    if isdir(gdir) == False:
+        makedirs(gdir)
+    chdir(gdir)
+
+    fnamet = fname
+    while isfile(fnamet+'.txt') == True:
+        fnamet = fnamet + ' (2)'
+    savetxt((fnamet+'.txt'),transpose(array([t,datos[:,0],datos[:,1]])),delimiter='\t',newline='\n')
+    
+    chdir(home)
