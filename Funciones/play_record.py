0ba18a6604848ec2cebe5c75af58f7a3006aae1d
0xInfty <anirix.vall@gmail.com>
2018-09-04 16:16:57 -0300

    Modifico función 'play_record'. Encuentro problemas. Cambios mayores de sintaxis.
    Cambios mayores de sintaxis:
    	para reducir el número de variables creo diccionarios y a partir de ellos armo espacios de variables;
    
    Problemas:
    	no me reproduce el sonido corriendo play_record(1).

--- Funciones/play_record.py
+++ Funciones/play_record.py
@@ -12,17 +12,13 @@ import os
 
 def play_record(
     T, # duración en seg de la grabación
-    dT = 1, # duración en seg de un buffer
-    dt = 0.005, # duración en seg de cada frame del buffer
-    ch = 1, # cantidad de canales
-    sr = 44000, # sampling rate de grabación
-    freq = 440, # frecuencia de tono en modo 'freq'
     form = 'freq', # modo de input "saw" o "sine"
     mode = 'txt',
     savetxt = False,
     showplot = True,
-    fname = 'output',
-    fdir = os.getcwd()
+    buffer = {'dT': 1, 'dt': 0.005},
+    audio = {'ch': 1, 'sr': 44000, 'freq': 440},
+    filendir = {'fname': 'output', 'fdir': os.getcwd()},
     ):
     
     """Reproduce una señal de sonido y graba por jack al mismo tiempo.
@@ -46,9 +42,26 @@ Además, si 'savetxt==True', guarda un archivo de texto. Y si \
     
     from new_name import new_name
     from waveform import waveform
+    from argparse import Namespace
 
     home = os.getcwd()
-        
+    
+    for key in ['dT', 'dt']:
+        if key not in buffer:
+            buffer.update({key: {'dT': 1, 'dt': 0.005}[key]})
+    for key in ['ch', 'sr', 'freq']:
+        if key not in audio:
+            audio.update({key: 
+                {'ch': 1, 'sr': 44000, 'freq': 440}[key]})
+    for key in ['fdir', 'fname']:
+        if key not in filendir:
+            filendir.update({key:
+                {'fname': 'output', 'fdir': os.getcwd()}[key]})
+    
+    bsp = Namespace(**buffer) # buffer space
+    asp = Namespace(**audio) # audio space
+    fsp = Namespace(**filendir) # filendir space
+    
     if mode != 'txt':
         print("Modo: 'wav'")
     else:
@@ -56,49 +69,53 @@ Además, si 'savetxt==True', guarda un archivo de texto. Y si \
         print("Guardar txt: %s" % savetxt)
         print("Mostrar gráfico: %s" % showplot)
     
-    print("Longitud del buffer: %i" % int(dT/dt))
-    if int(dT/dt) != dT/dt:
+    print("Longitud del buffer: %i" % int(bsp.dT/bsp.dt))
+    if int(bsp.dT/bsp.dt) != bsp.dT/bsp.dt:
         print("¡Ojo! El intervalo dt no entra un número entero \
         de veces en dT")
-        dt = dT/int(dT/dt)
-        print("Intervalo redefinido a %f s" % dt)
+        bsp.dt = bsp.dT/int(bsp.dT/bsp.dt)
+        print("Intervalo redefinido a %f s" % bsp.dt)
         
-    if int(T/dT) != T/dT:
+    if int(T/bsp.dT) != T/bsp.dT:
         print("¡Ojo! El tiempo de grabación \
         no es un número entero de veces dT")
-        if int(T/dT) != 0:
-            T = int(T/dT)*dT
+        if int(T/bsp.dT) != 0:
+            T = int(T/bsp.dT)*bsp.dT
         else:
             print("¡Ojo! El tiempo de grabación era menor a dT")
-            T = dT
+            T = bsp.dT
         print("Tiempo de grabación redefinido a %f s" % T)
     
     p = pyaudio.PyAudio()
     s_f = []
-    n = int(dT/dt)    
+    n = int(bsp.dT/bsp.dt)    
     
-    s_0 = waveform(form, n, freq=freq)    
+    s_0 = waveform(form, n, freq=asp.freq)
     s_0 = s_0.astype(np.float32)
     
     def callback(in_data, frame_count, time_info, status):
         return (s_0, pyaudio.paContinue)
     
-    streamplay = p.open(format=pyaudio.paFloat32,
-                channels=ch,
-                rate=sr,
-                output=True,
-                stream_callback = callback)
+    streamplay = p.open(
+                format = pyaudio.paFloat32,
+                channels = 1,
+                rate = asp.sr,
+                output = True,
+                stream_callback = callback,
+                )
 
-    streamrecord = p.open(format=pyaudio.paFloat32,
-                channels=ch,
-                rate=sr,
-                input=True,
-                frames_per_buffer=n)
+    streamrecord = p.open(
+                format = pyaudio.paFloat32,
+                channels = asp.ch,
+                rate = asp.sr,
+                input = True,
+                frames_per_buffer = n,
+                )
 
     streamplay.start_stream()
     print("* recording")
     streamrecord.start_stream()
-    data = streamrecord.read(int(sr * T))
+    data = streamrecord.read(int(asp.sr * T))
     print("* done recording")    
 
     streamrecord.stop_stream()
@@ -112,13 +129,13 @@ Además, si 'savetxt==True', guarda un archivo de texto. Y si \
     if mode == 'txt':
         s_f.extend(np.fromstring(data, 'Float32'))
         if savetxt:
-            os.chdir(fdir)
-            savetxt((new_name(fname, 'txt', fdir)+'.txt'), s_f)
+            os.chdir(fsp.fdir)
+            savetxt((new_name(fsp.fname, 'txt', fsp.fdir)+'.txt'), s_f)
             os.chdir(home)
         if showplot:
             plt.figure()
             #plt.plot(samples[2000:3000],'bo')
-            plt.plot(s_f[2000:3000],'ro')
+            plt.plot(s_f[2000:3000], 'ro-')
             plt.ylabel('señal grabada')
             plt.grid()
             plt.show()
@@ -126,12 +143,14 @@ Además, si 'savetxt==True', guarda un archivo de texto. Y si \
             
     else:
         s_f.append(data)
-        fname = new_name(fname, 'wav', fdir)
-        wf = wave.open((fname+'.wav'), 'wb')
-        wf.setnchannels(ch)
+        fsp.fname = new_name(fsp.fname, 'wav', fsp.fdir)
+        os.chdir(fsp.fdir)
+        wf = wave.open((fsp.fname + '.wav'), 'wb')
+        wf.setnchannels(asp.ch)
         wf.setsampwidth(p.get_sample_size(pyaudio.paFloat32))
-        wf.setframerate(sr)
+        wf.setframerate(asp.sr)
         wf.writeframes(b''.join(s_f))
         wf.close()
+        os.chdir(home)
 
     return s_0, s_f
