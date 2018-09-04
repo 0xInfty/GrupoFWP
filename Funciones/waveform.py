6f9a4306633914d351fcb180bdb1c87041f79a97
0xInfty <anirix.vall@gmail.com>
2018-09-04 02:37:16 -0300

    Modifico función 'waveform'. Realizo cambios:
    Cambios de funcionamiento leves:
    	agrego modos 'squ', 'saw' para cuadrada y pendiente (no onda triangular, sino serrucho).
    
    Cambios de funcionamiento importantes:
    	agrego modo 'freq' y kwargs* entre las variables para que pueda tomar 'freq'.
    
    Cambios de sintaxis menores:
    	modifico docstring según convención de numpy.

--- Funciones/waveform.py
+++ Funciones/waveform.py
@@ -5,51 +5,85 @@ Created on Wed Aug 29 19:49:50 2018
 @author: 0xInfty
 """
 
-def waveform(key, n):
+def waveform(key, n, **kwargs):
     """Makes a period of a wave of form 'key' on an array of lenght 'n'.
     
 This function defines a period of a wave. Its waveform is chosen with \
 the input 'key' which can take a limited number of values: 'sin' for \
-a sine, 'tri' for a triangle wave. Its digital resolution is given by \
-the input 'n' which is the number of values it takes in a period and \
-which is therefore the number of intervals the period is divided into.
+a sine, 'tri' for a triangle wave, 'saw' for increasing linear, 'squ' \
+for square wave and 'freq' for a sine with freq periods inside. Its \
+digital resolution is given by the input 'n' which is the number of \
+values it takes in a period and which is therefore the number of \
+intervals the period is divided into.
 
-Variables:
->> key (str) [waveform]
->> n (int) [resolution]
 
-Returns:
->> wave (array) [period of a wave, array of length n]
+Parameters
+----------
+key: str {'sin', 'tri', 'saw', 'freq', 'squ'}
+    Waveform
+n: int
+    Resolution
 
-Raises:
-- KeyError("Los posibles valores de key son: 'sin', 'tri'") if a wrong \
-waveform key is given.
+    
+Other parameters
+----------------
+freq: int, float
+    Frequency for 'freq' key.
 
-Beware:
+
+Returns
+-------
+wave: array
+    Period of a wave, array of length n
+
+
+Raises
+------
+- KeyError("Los posibles valores de key son: 'sin', 'tri', 'saw', \
+'freq', 'squ'") if a wrong waveform key is given.
+
+           
+Warnings
+--------
 - The returned waveform is normalized to 1 (its maximum amplitude is 1).
 - The returned waveform is centered on 0 (it's a symmetric function \
 whose minimum amplitude is -1).
     
     """
 
+    import numpy as np
+
     if key == 'sin':
-        
-        from numpy import sin, pi, arange
-        
-        return sin(2*pi*arange(n)/n)
+                
+        out = np.sin(2*np.pi*np.arange(n)/n)
 
     elif key == 'tri':
+                
+        out = np.zeros(n)
         
-        from numpy import zeros, linspace
+        out[0:n//2+1] = np.linspace(0,1,n//2+1)
+        out[n//2:n] = np.linspace(1,0,n//2+1)[0:n//2]
+    
+    elif key == 'freq':
         
-        out = zeros(n)
+        if 'freq' not in globals() and 'freq' not in locals():
+            freq = 440
         
-        out[0:n//2+1] = linspace(0,1,n//2+1)
-        out[n//2:n] = linspace(1,0,n//2+1)[0:n//2]
+        out = np.sin(2*np.pi*np.arange(n)*freq/n)
+    
+    elif key == 'squ':
+    
+        out = np.zeros(n)
         
-        return out
-
+        out[0:n//2+1] = np.ones(n//2+1)
+        out[n//2:n] = -np.ones(n//2+1)
+        
+    elif key == 'saw':
+        
+        out = np.linspace(0,1,n)
+        
     else:        
 
-        raise KeyError("Los posibles valores de key son: 'sin', 'tri'.")
+        raise KeyError("Los posibles valores de key son: 'sin', 'tri', \
+        'saw', 'freq', 'squ'.")
         return
