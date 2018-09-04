5251925c7690d2d91e9dba0c115a57969dcd60fd
0xInfty <anirix.vall@gmail.com>
2018-09-03 21:46:58 -0300

    Modifico la función 'play_record'. Cambios: mayores y menores de funcionamiento, graves y leves de sintaxis.
    Cambios graves de sintaxis:
    	cambio de nombre 'mode'	a 'form' para la variable que controla la forma de onda.
    
    Cambios importantes de funcionamiento:
    	reasigno la variable 'mode' para decidir si guardar 'txt' o 'wav' (me parece que no tenía sentido hacer ambas);
    	ahora si 'mode=="txt"', entonces realiza conversión a float;
    	si además 'savetxt' es True, entonces guarda en un archivo txt que lleva la frecuencia en el nombre';
    	si además 'showplot' es True, entonces muestra un gráfico de la señal;
    	ahora si en cambio 'mode=="wav"', entonces guarda un archivo wav.
    
    Cambios menores de funcionamiento:
    	quito variables 'savewav', 'returntxt' que habían quedado demás;
    	mergeo las variables 's_f' y 'recording_float' en un único 's_f' que toma o bien el valor original o bien el float dependiendo de si 'mode="txt"' o si 'mode="wav"'.
    
    Cambios leves de sintaxis:
    	modifico temas de espaciado.

--- Funciones/play_record.py
+++ Funciones/play_record.py
@@ -8,9 +8,10 @@ Created on Tue Aug 28 18:34:02 2018
 @author: mfar
 """
 
-from pyaudio import paInt16, paFloat32
+from pyaudio import paFloat32 # paInt16
 
-def play_record(fname='output', 
+def play_record(
+    fname = 'output',
     #A = 1, # amplitud, range [0,1]
     T = 1, # duración del buffer
     #n = 200 # cantidad de frames en un buffer = chunk
@@ -20,27 +21,33 @@ def play_record(fname='output',
     ch = 2, # cantidad de canales tanto de input como de output
     ft = paFloat32, # formato de input
     freq = 440,
-    mode = 'saw', # modo de input "saw" o "sine"
-    savewav = True,
-    savetext = False,
-    returntxt = False): 
+    form = 'saw', # modo de input "saw" o "sine"
+    mode = 'txt',
+    savetxt = False,
+    showplot = True,
+    ):
 
     import pyaudio
-    from numpy import linspace, float32, fromstring,savetxt
+    from numpy import linspace, float32, fromstring, savetxt
     import wave
     import matplotlib.pyplot as plt
-
-    p = pyaudio.PyAudio()
+   
+    if mode != 'txt':
+        print("Modo: 'wav'")
+    else:
+        print("Modo: 'txt'")
+        print("Guardar txt: %s" % savetxt)
+        print("Mostrar gráfico: %s" % showplot)
     
-    s_f = []
-    recording_float=[]
     print("Longitud del buffer: %i" % int(T/dt))
     if int(T/dt) != T/dt:
         print("¡Ojo! El intervalo dt no entra un número entero de veces en T")
         dt = T/int(T/dt)
         print("Intervalo redefinido a %f s" % dt)
     
-    n = int(T/dt)
+    p = pyaudio.PyAudio()
+    s_f = []
+    n = int(T/dt)    
     
     if mode=='sine':
         from numpy import sin, pi
@@ -59,7 +66,6 @@ def play_record(fname='output',
         
         s_0[0:n//2+1] = linspace(0,1,n//2+1)
         s_0[n//2:n] = linspace(1,0,n//2+1)[0:n//2]
-    
     
     else:
         return "¡Error! Modo de onda de output no especificada"
@@ -84,36 +90,36 @@ def play_record(fname='output',
     streamplay.start_stream()
     print("* recording")
     streamrecord.start_stream()
-    data = streamrecord.read(n*(sr / n * m * T))
-    recording_float.extend(fromstring(data, 'Float32'))
-    s_f.append(data)
+    data = streamrecord.read(n * (sr/n * m * T))
     print("* done recording")    
-    
 
     streamrecord.stop_stream()
     streamplay.stop_stream()
     
     streamrecord.close()
     streamplay.close()
-       
+
     p.terminate()
-    
-    savetxt(str(freq)+'.txt',recording_float)
-    
-    
-    #graficar pa ver que sale
-    plt.figure()
-    #plt.plot(samples[2000:3000],'bo')
-    plt.plot(recording_float[2000:3000],'ro')
-    plt.ylabel('señal grabada')
-    plt.grid()
-    plt.show()    
-        
-    
-    wf = wave.open((fname+'.wav'), 'wb')
-    wf.setnchannels(ch)
-    wf.setsampwidth(p.get_sample_size(ft))
-    wf.setframerate(sr)
-    wf.writeframes(b''.join(s_f))
-    wf.close()
-    return s_0, recording_float
+
+    if mode == 'txt':
+        s_f.extend(fromstring(data, 'Float32'))
+        if savetxt:
+            savetxt(str(freq)+'.txt', s_f)
+        if showplot:
+            plt.figure()
+            #plt.plot(samples[2000:3000],'bo')
+            plt.plot(s_f[2000:3000],'ro')
+            plt.ylabel('señal grabada')
+            plt.grid()
+            plt.show()
+            
+    else:
+        s_f.append(data)   
+        wf = wave.open((fname+'.wav'), 'wb')
+        wf.setnchannels(ch)
+        wf.setsampwidth(p.get_sample_size(ft))
+        wf.setframerate(sr)
+        wf.writeframes(b''.join(s_f))
+        wf.close()
+
+    return s_0, s_f
