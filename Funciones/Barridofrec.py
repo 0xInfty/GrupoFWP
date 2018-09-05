5493471e99a0061a6ac87c5bf94249b0b34b3419
0xInfty <anirix.vall@gmail.com>
2018-09-05 01:06:28 -0300

    Modifico función en 'Barridofreq.py'. Cambios leves de sintaxis e importantes de funcionamiento.
    Cambios de sintaxis leves:
    	cambio el nombre de la función para que sea corto y se pueda llamar así también el archivo;
    	modifico la estructura del for para que sea más pythonica;
    
    Cambios importantes de funcionamiento:
    	modifico los parámetros de 'play_record' para que esté al día;
    	pido que la función devuelva datos en arrays;
    	pido que si 'savetxt=True', guarde esos datos en un archivo txt.

--- Funciones/Barridofrec.py
+++ Funciones/Barridofrec.py
@@ -5,19 +5,54 @@ Created on Mon Sep  3 17:40:46 2018
 @author: mfar
 """
 import numpy as np
-def barrido_en_frecuencias(freq_ini, freq_fin, totalnumber):
-    frecuenciasparabarrer=np.linspace(freq_ini, freq_fin, totalnumber)
-    from play_record import  play_record
+
+def freq_sweep(freq_ini, freq_fin, totalnumber, \
+                           savetxt=False):
+    
+    from play_record import play_record
+    from pyaudio import paFloat32
     import matplotlib.pyplot as plt
-    rms_frec=np.zeros(len(frecuenciasparabarrer))
-    for i in range(len(frecuenciasparabarrer)):
-        s_0, recording=play_record(freq=frecuenciasparabarrer[i])
-        rms_s0=np.sqrt(np.mean((s_0)**2))
-        rms_recording=np.sqrt(np.mean((np.array(recording))**2))
-        rms_frec[i]=rms_recording/rms_s0
+    import rms as rms
+    
+    freqs = np.linspace(freq_ini, freq_fin, totalnumber)
+    rms_freq = np.zeros(totalnumber)
+    
+    i = 0
+    for freq in freqs:
+        s_0, recording = play_record(
+                1,
+                mode='txt', 
+                form='freq',
+                savetxt=False,
+                showplot=False,
+                buffer={'freq': freq},
+                audio={'ch_play': 1, 'ch_rec': 1,
+                       'ft_play': paFloat32, 'ft_rec': paFloat32}
+                )
+        rms_s0 = rms(s_0)
+        rms_recording = rms(recording)
+        rms_freq[i] = rms_recording/rms_s0
+        i = i + 1
+    
+    rms_dec = 10*np.log10(rms_freq)
+    
     plt.figure()
     #plt.plot(samples[2000:3000],'bo')
-    plt.plot(frecuenciasparabarrer,10*np.log10(rms_frec),'b-')
+    plt.plot(freqs, rms_dec, 'b-')
     plt.ylabel('bandwith')
     plt.grid()
     plt.show()
+    
+    if savetxt:
+        import os
+        from new_name import new_name
+        fname = 'barrido_freq'
+        fname = new_name(fname, '.txt', os.getcwd())
+        np.savetxt(
+            (fname+'.txt'),
+            np.transpose(np.array([freqs, rms_freq, rms_dec])),
+            delimiter='\t',
+            newline='\n',
+            )
+    
+    return freqs, rms_freq, rms_dec
