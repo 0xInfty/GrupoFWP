51339073d1573c4cadde9ea0edb306f32873ea31
Monica Far <mfar@puesto3.cuartos.inv.dc.uba.ar>
2018-09-10 15:36:33 -0300

    Comente y saque las cosas de mas del script de lectoescritura

--- versionesViejas/lectoescritura.py
+++ versionesViejas/lectoescritura.py
@@ -5,11 +5,43 @@ Created on Tue Sep  4 14:53:10 2018
 @author: mfar
 """
 
-from stereo import encode, decode
-
 import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+
+def decode(in_data, channels):
+    import numpy as np
+
+    """
+    Convert a byte stream into a 2D numpy array with 
+    shape (chunk_size, channels)
+
+    Samples are interleaved, so for a stereo stream with left channel 
+    of [L0, L1, L2, ...] and right channel of [R0, R1, R2, ...], the output 
+    is ordered as [L0, R0, L1, R1, ...]
+    """
+    result = np.fromstring(in_data, dtype=np.float32)
+
+    chunk_length = len(result) / channels
+    assert chunk_length == int(chunk_length)
+
+    result = np.reshape(result, (int(chunk_length), channels))
+    return result
+
+
+def encode(signal):
+    import numpy as np
+
+    """
+    Convert a 2D numpy array into a byte stream for PyAudio
+
+    Signal should be a numpy array with shape (chunk_size, channels)
+    """
+    interleaved = signal.flatten()
+    out_data = interleaved.astype(np.float32).tostring()
+    return out_data
 
 
 
@@ -29,21 +61,18 @@ RECORD_SECONDS = 3
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 
-# generate samples, note conversion to float32 array
-samplesleft =(np.sin(2*np.pi*np.arange(RATE*duration)*(f*3)/RATE)).astype(np.float32)
-samplesright = (np.sin(2*np.pi*np.arange(RATE*duration)*f/RATE)).astype(np.float32)
-samplestuple = [np.transpose(samplesleft), np.transpose(samplesright)]
-#samplestuple = [samplesleft, samplesright]
-samplesarray=np.transpose(np.array(samplestuple))
-a=samplesarray.flatten()
-samples=encode(samplesarray)
-#samples=encode(np.transpose(samplesarray))
+samplesleft =(np.sin(2*np.pi*np.arange(RATE*duration)*(f*3)/RATE)).astype(np.float32) #lo que quiero en el canal de la izquierda
+samplesright = (np.sin(2*np.pi*np.arange(RATE*duration)*f/RATE)).astype(np.float32) # lo que quiero en el canal de la derecha
+samplestuple = [np.transpose(samplesleft), np.transpose(samplesright)] # me armo una tupla que tenga en cada columna lo que quiero reproducir por cada canal
+samplesarray=np.transpose(np.array(samplestuple)) # la paso a array, y la traspongo para que este en el formato correcto de la funcion de encode
+samples=encode(samplesarray) #formateo los datos para que puedan ser interpretados por el modulo pyaudio
 
+#me armo el callback que simplemente reproduce la señal
 def recordsignal(in_data, frame_count, time_info, status):
     outputdata = samples
     return (outputdata, pyaudio.paContinue)
 
-
+#me armo el stream de reproduccion con los parametros definidos al principio
 streamplay = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
@@ -51,9 +80,10 @@ streamplay = p.open(format=FORMAT,
                 stream_callback = recordsignal)
 
 
-# start the stream (4)
+# Le doy play
 streamplay.start_stream()
 
+#me armo el stream de grabacion
 streamrecord = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
@@ -70,53 +100,35 @@ streamrecord = p.open(format=FORMAT,
 
 
 
-frames = []
-recording_float=[]
+frames = [] #este va a tener la data tal cual se graba, para despues hacer un wav
+recording_float=[] #este me pasa lo grabado a array
 print("* recording")
-data = streamrecord.read(CHUNK*(RATE / CHUNK * RECORD_SECONDS))
-recording_float.extend(np.fromstring(data, 'Float32'))
+data = streamrecord.read(RATE * RECORD_SECONDS)#grabo todo lo que hay que grabar
+recording_float.extend(np.fromstring(data, 'Float32'))#paso a array lo grabado
 frames.append(data)
 print("* done recording")
 
 
 
-
-
-
-#while streamplay.is_active():
-#   time.sleep(0.1)
-   
+#cierro el stream de grabacion y de reproduccion
 streamrecord.stop_stream()
 streamrecord.close()
 
-# wait for stream to finish (5)
-
-
 streamplay.stop_stream()
 streamplay.close()
-
     
 p.terminate()
 
+#paso lo grabado a un array de dos columnas, en cada una de ellas un canal
 result= decode(data, 2)
 
 
 #graficar pa ver que sale
 plt.figure()
-plt.plot(samplesarray[2000:2100,0]/10,'m-')
-plt.plot(samplesarray[2000:2100,1]/10,'k-')
-#plt.plot(np.array(recording_float[2000:2100]),'g-')
-plt.plot(result[2000:2100,0],'r-')
-plt.plot(result[2000:2100,1],'b-')
-#plt.plot(samplesright[2000:2100],'m-')
-#plt.plot(samplesleft[2000:2100],'k-')
+plt.plot(samplesarray[3000:3100,0]/10,'m-')
+plt.plot(samplesarray[3000:3100,1]/10,'k-')
+plt.plot(result[3000:3100,0],'r-')
+plt.plot(result[3000:3100,1],'b-')
 plt.ylabel('señal grabada')
 plt.grid()
 plt.show()
-
-
-
-#usando la funcion de vale
-#for i in range[len(frecuencias)]:
-#    play_record(freq=frecuencias [i])
-#    np.savetxt('archivo%01d.txt' % i,frames)
