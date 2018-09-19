4295dbc59ab4ffdfa033bcf0731bc1bd13b9269e
mwappner <42619775+mwappner@users.noreply.github.com>
2018-09-19 19:04:50 -0300

    Implementad y funcional: frequency sweep con generadores

--- fwp_medir.py
+++ fwp_medir.py
@@ -47,7 +47,7 @@ thesignal = fwp.play_callback_rec(signal_to_play,
 #Some configurations
 after_record_do = fwp.AfterRecording(savewav = False, showplot = True,
                                      saveplot = False, savetext = False)                                     
-duration = 1
+duration = 5
 nchannelsrec = 2
 nchannelsplay = 2
 signal_freq = 2000
@@ -63,12 +63,12 @@ signal_to_play = signalmaker.write_generator((seno1,cuadrada), duration=duration
 #NOTE: to write two different signals in two channels use tuples: (wave1,wave2)
 
 thesignal = fwp.play_callback_rec_gen(signal_to_play, 
-                                  duration,
+                                  duration*2,
                                   nchannelsplay=nchannelsplay,
                                   nchannelsrec=nchannelsrec,
                                   after_recording=after_record_do)
 
-#%% Frequency sweep
+#%% Frequency sweep using generators
 
 freq_start = 50
 freq_stop = 22000
@@ -77,70 +77,9 @@ freq_step = 50
 # Some configurations
 after_record_do = fwp.AfterRecording(savewav = False, showplot = False,
                                      saveplot = False, savetext = True) 
-nchannelsrec = 1
-nchannelsplay = 2 # Cause of cable issues
-name = 'Freq_Sweep'
-
-seno = wmaker.Wave('sine') # Default frequency and amplitude.
-signalmaker = paw.PyAudioWave(nchannels=nchannelsplay) # Default srate
-
-# Frequencies and durations
-frequencies = np.arange(freq_start, freq_stop, freq_step)
-durations = np.array([100/freq for freq in frequencies]) # 100 periods
-
-# If non existent, create directory to save to
-savedir = sav.new_dir(os.path.join(os.getcwd(), name))
-filename = os.path.join(savedir, name)
-makefile = lambda freq : '{}_{:.0f}_Hz'.format(filename, freq)
-
-signalrms = []
-
-for freq, dur in zip(frequencies, durations):
-    
-    # Set up stuff for this frequency
-    seno.frequency = freq
-    signal_to_play = signalmaker.write_signal(seno, 
-                                              periods_per_chunk=10000, 
-                                              display_warnings=False)
-    after_record_do.filename = makefile(freq)
-    
-    # Play, record and process
-    thesignal = fwp.play_callback_rec(signal_to_play, 
-                                      recording_duration=dur,
-                                      nchannelsplay=nchannelsplay,
-                                      nchannelsrec=nchannelsrec,
-                                      after_recording=after_record_do)
-    
-    signalrms.append(rms.rms(thesignal))
-    
-del thesignal
-
-signalrms = np.array(signalrms)
-signaldec = 10*np.log10(signalrms/max(signalrms))
-
-plt.figure()
-plt.plot(frequencies, signaldec, 'b-')
-plt.ylabel('Decibels')
-plt.xlabel('Frequency (Hz)')
-plt.grid()
-plt.show() 
-
-sav.saveplot('{}_Plot.pdf'.format(filename))
-sav.savetext(np.transpose(np.array(frequencies, signalrms, signaldec)),
-             '{}_Data.txt'.format(filename))
-
-#%% Frequency sweep using generators
-
-freq_start = 500
-freq_stop = 22000
-freq_step = 250
-
-# Some configurations
-after_record_do = fwp.AfterRecording(savewav = False, showplot = False,
-                                     saveplot = False, savetext = True) 
 nchannelsrec = 2
 nchannelsplay = 2 # Cause of cable issues
-name = os.path.join('Measurements','Freq_Sweep')
+name = 'Freq_Sweep'
 
 seno = wmaker.Wave('sine') # Default frequency and amplitude.
 signalmaker = paw.PyAudioWave(nchannels=nchannelsplay) # Default srate
@@ -152,7 +91,7 @@ duration = 50/frequencies[0] #play 50 periods of slowest wave
 durations = [duration] * len(frequencies)
 
 # If non existent, create directory to save to
-savedir = sav.new_dir(os.path.join(os.getcwd(), name))
+savedir = sav.new_dir(os.path.join(os.getcwd(), 'Measurements', name))
 filename = os.path.join(savedir, name)
 makefile = lambda freq : '{}_{:.0f}_Hz'.format(filename, freq)
 
@@ -188,7 +127,7 @@ plt.grid()
 plt.show() 
 
 sav.saveplot('{}_Plot.pdf'.format(filename))
-sav.savetext(np.transpose(np.array(frequencies, signalrms, signaldec)),
+sav.savetext(np.transpose(np.array([frequencies, signalrms, signaldec])),
              '{}_Data.txt'.format(filename))
 
 #%% Calibrate playing
