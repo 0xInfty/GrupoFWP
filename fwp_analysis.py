1dde86f176c9166f64f0999afae21774f2dabdd1
0xInfty <anirix.vall@gmail.com>
2018-10-06 23:14:58 -0300

    Módulo 'fwp_analysis' ['linear_fit'].
    	Agrego función 'linear_fit' que realiza un ajuste lineal con numpy.polifit y lo plotea.

--- fwp_analysis.py
+++ fwp_analysis.py
@@ -2,11 +2,13 @@
 """
 Created on Sat Oct  6 17:09:13 2018
 
-@author: Usuario
+@author: Vall
 """
 
+import fwp_format as fmt
+import matplotlib.pyplot as plt
+from math import sqrt
 import numpy as np
-import math as mth
        
 #%%
 
@@ -58,185 +60,158 @@ def mean(X, dX=None):
 
         mean = np.average(X, weights=1/dX**2)
         variance = np.average((X-mean)**2, weights=1/dX**2)
-        return (mean, mth.sqrt(variance))
+        return (mean, sqrt(variance))
     
     else:
         return (np.mean(X), np.std(X))
 
-
 #%%
 
-def error_value(X, nc=0, units='',
-                scale=True, lscale=False, rnum=False, fprec=True):
-
-    ans = '%.' + str(nc-1) + 'e'
-    ans = ans % X[1]
-    ans = ans.split("e")
+def linear_fit(X, Y, dY=None, showplot=True,
+               plot_some_errors=(False, 20), **kwargs):
+    """Applies linear fit and returns m, b and Rsq. Can also plot it.
     
-    orderr = int(ans[1])
-    err = ans[0]
-
-    ans2 = '%15e' % X[0]
-    ans2 = ans2.split("e")
+    By default, it applies minimum-square linear fit. If dY is 
+    specified, it applies weighted minimum-square linear fit.
     
-    ordnum = int(ans2[1])
-    num = float(ans2[0])
+    Parameters
+    ----------
+    X : np.array, list
+        Independent X data to fit.
+    Y : np-array, list
+        Dependent Y data to fit.
+    dY : np-array, list
+        Dependent Y data's associated error.
+    shoplot : bool
+        Says whether to plot or not.
+    plot_some_errors : tuple (bool, int)
+        Says wehther to plot only some error bars (bool) and specifies 
+        the number of error bars to plot.
     
-    if -9<=ordnum<12 and scale==True:
-        pref=['n', r'$\mu$', 'm','','k','M','G']
-        escala=[-9,-6,-3,0,3,6,9,12]
-        for i in range(7):
-            if lscale==False:
-                if escala[i]<=ordnum<escala[i+1]:
-                    pref=pref[i]
-                    escala=escala[i]
-                    break
-            else:
-                if escala[i]-1<=ordnum<escala[i+1]-1:
-                    pref=pref[i]
-                    escala=escala[i]
-                    break
-        ans=True
-    else:
-        escala=ordnum
-        pref=''
-        ans=False
-
-    if orderr<escala:
-        if nc-orderr+escala-1>=0:
-            ans3 = '%.' + str(nc-orderr+escala-1) + 'f'
-            err = ans3 % ( float(err)*10**(orderr-escala) )
-            num = ans3 % ( num*10**(ordnum-escala) )
-        else:
-            err = float(err)*10**(orderr-escala)
-            num = float(num)*10**(ordnum-escala)
-    else:
-        ans3 = '%.' + str(nc-1) + 'f'
-        err = ans3 % ( float(err)*10**(orderr-escala) )
-        num = ans3 % ( float(num)*10**(ordnum-escala) )
-                
-    if ans==False and ordnum!=0:
-        leg = r'(%s$\pm$%s)$10^{%i}$%s%s' % (num,err,escala,pref,units)
-    else:
-        leg = r'(%s$\pm$%s)%s%s' % (num,err,pref,units)
+    Returns
+    -------
+    (m, dm) : tuple (float, float)
+        Linear fit's slope: value and associated error.
+    (b, db) : tuple (float, float)
+        Linear fit's intercept: value and associated error.
+    rsq : float
+        Linear fit's R Square Coefficient.
     
-    if rnum==True:
-        print(leg)
-        if fprec==True:
-            return X
-        else:
-            return ( float(num)*10**escala, float(err)*10**escala )
-    else:
-        return leg
-
-#%%
-
-def Ajuste_Polyfit(X,Y,dY=0,\
-                tit='Ajuste lineal Polyfit', ylab='nan', xlab='nan', \
-                yft='nan', xft='nan', dim='nan', \
-                xaxlim='nan', yaxlim='nan', \
-                text='nan', textp='up', \
-                leg='nan', legp = 'best', mbp='nan', \
-                ajunits = ('',''), ajnc=2, ajrnc=3, ajscale=(False,False), \
-                ernum=(False,20), \
-                fname='nan', ftype='png', \
-                showfig=True, savefigure=False, savetext=False, figinforme=True, \
-                gdir='C:\\Users\\Usuario\\Documents\\Git\\Privado'):  
+    Other Parameters
+    ----------------
+    txt_position : tuple (horizontal, vertical), optional
+        Indicates the parameters' text position. Each of its values 
+        should be a number (distance in points measured on figure). 
+        But vertical value can also be 'up' or 'down'.
+    mb_units : tuple (m_units, b_units), optional
+        Indicates the parameter's units. Each of its values should be a 
+        string.
+    mb_error_digits : tuple (m_error_digits, b_error_digits), optional
+        Indicates the number of digits to print in the parameters' 
+        associated error. Default is 3 for slope 'm' and 2 for intercept 
+        'b'.
+    mb_string_scale : tuple (m_string_scale, b_string_scale), optional
+        Indicates whether to apply string prefix's scale to printed 
+        parameters. Each of its values should be a bool; i.e.: 'True' 
+        means 'm=1230.03 V' with 'dm = 10.32 V' would be printed as 
+        'm = (1.230 + 0.010) V'. Default is '(False, False)'.
+    rsq_decimal_digits : int, optional.
+        Indicates the number of digits to print in the Rsq. Default: 3.
+        
+    Warnings
+    --------
+    The returned Rsq doesn't take dY weights into account.
+    
+    """
 
     # ¿Cómo hago Rsq si tengo pesos?
     
-    from numpy import ndarray, polyfit#, corrcoef
-    from math import sqrt
-    from GAux import Ajuste_2D, Ajuste_Ponderado_2D
-    from FAux import Rsq
-
-    if type(dY)==int:
+    if dY is None:
         W = None
-        mod = 'Ajuste lineal'
-    elif type(dY)!=ndarray:
-        print("¡Error! dY debe ser numpy.ndarray.")
-        return
     else:
-        W = abs(1/dY)
-        mod = 'Ajuste lineal ponderado'
-        
-    Data = polyfit(X,Y,1,cov=True,w=W)
+        W = 1/dY**2
+                
+    fit_data = np.polyfit(X,Y,1,cov=True,w=W)
     
-    m = ( Data[0][0], sqrt(Data[1][0,0]) )
-    b = ( Data[0][1], sqrt(Data[1][1,1]) )
-    rsq = Rsq(X,Y,m,b)
-    #rsq = ( corrcoef(X, Y)[0,1] )**2
+    m = fit_data[0][0]
+    dm = sqrt(fit_data[1][0,0])
+    b = fit_data[0][1]
+    db = sqrt(fit_data[1][1,1])
+    rsq = 1 - sum( (Y - m*X - b)**2 )/sum( (Y - np.mean(Y))**2 )
 
-    if m[0]>=0:
-        legp = 'lower right'
-        mbp = (.02,.9)
-    else:
-        legp = 'upper right'
-        mbp = (.02,.03)
-    
-    if showfig==True:
-        if mod == 'Ajuste lineal':
-            Ajuste_2D(Y=Y, X=X, m=m, b=b, rsq=rsq,  \
-                tit=tit, ylab=ylab, xlab=xlab, \
-                yft=yft, xft=xft, dim=dim, \
-                xaxlim=xaxlim, yaxlim=yaxlim, \
-                text=text, textp=textp, \
-                leg='nan', legp=legp, mbp=mbp, \
-                lstyle='', lmarksize=6, lwidth=1.0, lmark='.', lcolor='b', \
-                ajline = '-r', ajunits = ajunits, ajnc=ajnc, ajrnc=ajrnc, ajscale=ajscale, \
-                fname=fname, ftype=ftype, \
-                savefigure=savefigure, savetext=savetext, figinforme=figinforme, \
-                gdir=gdir)
+    try:
+        kwargs['txt_position']
+    except KeyError:
+        if m > 1:
+            aux = 'up'
         else:
-            Ajuste_Ponderado_2D(Y=Y,X=X,dY=dY,m=m,b=b,rsq=rsq,\
-                tit=tit, ylab=ylab, xlab=xlab, \
-                yft=yft, xft=xft, dim=dim, \
-                xaxlim=xaxlim, yaxlim=yaxlim, \
-                text=text, textp=textp, \
-                leg='nan', legp=legp, mbp=mbp, \
-                lstyle='', lmarksize=6, lwidth=1.0, lmark='.', lcolor='b', \
-                ercolor='black', ernum=ernum, \
-                ajline = '-r', ajunits = ajunits, ajnc=ajnc, ajrnc=ajrnc, ajscale=ajscale, \
-                fname=fname, ftype=ftype, \
-                savefigure=savefigure, savetext=savetext, figinforme=figinforme, \
-                gdir=gdir)
+            aux = 'down'
+        kwargs['txt_position'][0] = .02
 
-    return m, b, rsq
+    if showplot:
 
-#%%
+        plt.figure()
+        if dY is None:
+            plt.plot(X, Y, 'b.', zorder=0)
+        else:
+            if plot_some_errors[0] == False:
+                plt.errorbar(X, Y, yerr=dY, linestyle='b', marker='.',
+                             ecolor='b', elinewidth=1.5, zorder=0)
+            else:
+                plt.errorbar(X, Y, yerr=dY, linestyle='-', marker='.',
+                             color='b', ecolor='b', elinewidth=1.5,
+                             errorevery=len(Y)/plot_some_errors[1], 
+                             zorder=0)
+        plt.plot(X, m*X+b, 'r-', zorder=100)
+        
+        plt.legend(["Ajuste lineal ponderado","Datos"])
+        
+        kwargs_list = ['txt_position', 'mb_units', 'mb_string_scale', 
+                       'mb_error_digits', 'rsq_decimal_digits']
+        kwargs_default = [(.02, aux), ('', ''), (False, False), 
+                          (3, 2), 3]
+        for key, value in zip(kwargs_list, kwargs_default):
+            try:
+                kwargs[key]
+            except KeyError:
+                kwargs[key] = value
+        
+        if kwargs['text_position'][1] == 'up':
+            vertical = [.9, .82, .76]
+        elif kwargs['text_position'][1] == 'down':
+            vertical = [.05, .13, .21]
+        else:
+            if kwargs['text_position'][1] <= .08:
+                fact = .08
+            else:
+                fact = -.08
+            vertical = [kwargs['txt_position']+fact*i for i in range(2)]
+        
+        plt.annotate('m={}'.format(fmt.error_value(
+                        m, 
+                        dm,
+                        errordigits=kwargs['mb_error_digits'][0],
+                        units=kwargs['mb_units'][0],
+                        string_scale=kwargs['mb_string_scale'][0],
+                        one_point_scale=True)),
+                    (kwargs['text_position'][0], vertical[0]),
+                    xycoords='axes fraction')
+        plt.annotate('b={}'.format(fmt.error_value(
+                        b, 
+                        db,
+                        errordigits=kwargs['mb_error_digits'][1],
+                        units=kwargs['mb_units'][1],
+                        string_scale=kwargs['mb_string_scale'][1],
+                        one_point_scale=True)),
+                    (kwargs['text_position'][0], vertical[1]),
+                    xycoords='axes fraction')
+        rsqft = r'$R^2$={:.' + str(kwargs['rsq_decimal_digits']) + 'f}'
+        plt.annotate(rsqft.format(rsq),
+                    (kwargs['text_position'][0], vertical[2]),
+                    xycoords='axes fraction')
+        
+        fmt.plot_style()
+        plt.show()
 
-#def Rsq(X,Y):
-#
-#	from math import sqrt
-#
-#	n = len(X)
-#
-#	r = n*(sum(X*Y))-sum(X)*sum(Y)
-#	r = r/sqrt( (n*sum(X**2)-sum(X)**2)*(n*sum(Y**2)-sum(Y)**2) )
-#
-#	return r**2
+    return (m, dm), (b, db), rsq
 
-def Rsq(X,Y,m,b):
-# Solía ser Rsq_2
-    
-    print("¡Ojo! El rsq no tiene en cuenta los pesos.")
-    
-    from numpy import mean
-
-    try:
-        m = m[0]
-    except:
-        m = m
-    
-    try:
-        b = b[0]
-    except:
-        b = b
-
-    SSreg = sum( (Y - m*X - b)**2 )
-    SStot = sum( (Y - mean(Y))**2 )
-    rsq = 1 - SSreg/SStot
-    
-    # Con los pesos W=1/dY ¿multiplico W*(blah)**2 o hago (W*Y-blah)**2?
-
-    return rsq
